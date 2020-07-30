d4d34947025220abd3e811c18a74801abe3c9fd1
takuya.okita <trainingdayokita@gmail.com>
2020-07-30 09:57:58 +0000

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                     
                     <img class='navbar-logo' src="{{ asset('images/logo.png') }}">
                     
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'MvieReviews') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
